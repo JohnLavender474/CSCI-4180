51f2d681c08cba65600e6565f56cb3055a235c46
JohnLavender474 <jml62608@uga.edu>
2023-01-25 22:09:34 -0500

    Line basic algo

--- core/src/com/csci4810/Homework1.java
+++ core/src/com/csci4810/Homework1.java
@@ -1,6 +1,8 @@
 package com.csci4810;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,12 +11,32 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Homework1 extends ApplicationAdapter {
 
-    public record Line(int x0, int y0, int x1, int y1) {
+    public static class Line {
+
+        public final int x0;
+        public final int y0;
+        public final int x1;
+        public final int y1;
+
+        public Line(int x0, int y0, int x1, int y1) {
+            if (x0 < x1) {
+                this.x0 = x0;
+                this.y0 = y0;
+                this.x1 = x1;
+                this.y1 = y1;
+            } else {
+                this.x0 = x1;
+                this.y0 = y1;
+                this.x1 = x0;
+                this.y1 = y0;
+            }
+        }
+
     }
 
     public static class Grid {
@@ -26,9 +48,11 @@ public class Homework1 extends ApplicationAdapter {
         public final float gridY;
         public final Color gridColor;
         public final Color fillColor;
+        public final Color lineColor;
         public final List<Line> lines;
 
-        public Grid(float ppm, float gridX, float gridY, int width, int height, Color gridColor, Color fillColor) {
+        public Grid(float ppm, float gridX, float gridY, int width, int height,
+                    Color gridColor, Color fillColor, Color lineColor) {
             this.ppm = ppm;
             this.gridX = gridX;
             this.gridY = gridY;
@@ -36,7 +60,8 @@ public class Homework1 extends ApplicationAdapter {
             this.height = height;
             this.gridColor = gridColor;
             this.fillColor = fillColor;
-            lines = new LinkedList<>();
+            this.lineColor = lineColor;
+            lines = new ArrayList<>();
         }
 
         public void add(Line line) {
@@ -62,8 +87,8 @@ public class Homework1 extends ApplicationAdapter {
         }
 
         public void drawDirectLines(ShapeRenderer renderer) {
+            renderer.setColor(lineColor);
             for (Line l : lines) {
-                renderer.setColor(Color.PURPLE);
                 float offset = ppm / 2f;
                 renderer.line(
                         gridX + l.x0 * ppm + offset,
@@ -75,24 +100,48 @@ public class Homework1 extends ApplicationAdapter {
         }
 
         public void drawLinesBasic(ShapeRenderer renderer) {
+            renderer.setColor(fillColor);
             for (Line l : lines) {
-                renderer.setColor(fillColor);
-                float deltaX = Math.abs(l.x1 - l.x0);
-                float deltaY = Math.abs(l.y1 - l.y0);
-                float m = deltaY / deltaX;
-                for (int i = 0; i <= deltaX; i++) {
-                    float x = l.x0 + i;
-                    float y = Math.round(m * i + l.y0);
-                    renderer.rect(gridX + x * ppm, gridY + y * ppm, ppm, ppm);
+                float deltaX = l.x1 - l.x0;
+                float deltaY = l.y1 - l.y0;
+                if (deltaX > deltaY) {
+                    float m = deltaY / deltaX;
+                    for (int i = 0; i <= deltaX; i++) {
+                        float x = l.x0 + i;
+                        float y = Math.round(m * i + l.y0);
+                        renderer.rect(gridX + x * ppm, gridY + y * ppm, ppm, ppm);
+                    }
+                } else {
+                    float m = deltaX / deltaY;
+                    for (int i = 0; i <= deltaY; i++) {
+                        float y = l.y0 + i;
+                        float x = Math.round(m * i + l.x0);
+                        renderer.rect(gridX + x * ppm, gridY + y * ppm, ppm, ppm);
+                    }
                 }
             }
         }
 
         public void drawLinesBresenham(ShapeRenderer renderer) {
+            renderer.setColor(fillColor);
             for (Line l : lines) {
-                renderer.setColor(fillColor);
-                float deltaX = Math.abs(l.x1 - l.x0);
-                float deltaY = Math.abs(l.y1 - l.y0);
+                int dx = l.x1 - l.x0;
+                int dy = l.y1 - l.y0;
+                int inc1 = 2 * dy;
+                int inc2 = 2 * (dy - dx);
+                int e = 2 * dy - dx;
+                int x = l.x0;
+                int y = l.y0;
+                while (x <= l.x1) {
+                    renderer.rect(gridX + x * ppm, gridY + y * ppm, ppm, ppm);
+                    if (e >= 0) {
+                        y++;
+                        e += inc2;
+                    } else {
+                        e += inc1;
+                    }
+                    x++;
+                }
             }
         }
 
@@ -104,10 +153,12 @@ public class Homework1 extends ApplicationAdapter {
 
     public static final int GRID_WIDTH = 16;
     public static final int GRID_HEIGHT = 14;
+
     public static final Color GRID_COLOR = Color.RED;
     public static final Color FILL_COLOR = Color.BLUE;
+    public static final Color LINE_COLOR = Color.PURPLE;
 
-    public static final int NUM_LINES = 3;
+    public static final int NUM_LINES = 1;
 
     public Grid grid;
     public BitmapFont font;
@@ -126,33 +177,51 @@ public class Homework1 extends ApplicationAdapter {
         viewport.getCamera().position.y = SCREEN_HEIGHT * PPM / 2f;
         float x = (float) SCREEN_WIDTH / (float) GRID_WIDTH;
         float y = (float) SCREEN_HEIGHT / (float) GRID_HEIGHT;
-        grid = new Grid(PPM, x * PPM, y * PPM, GRID_WIDTH, GRID_HEIGHT, GRID_COLOR, FILL_COLOR);
+        grid = new Grid(PPM, x * PPM, y * PPM, GRID_WIDTH, GRID_HEIGHT, GRID_COLOR, FILL_COLOR, LINE_COLOR);
+
+        grid.add(new Line(3, 3, 7, 5));
+        grid.add(new Line(3, 12, 8, 7));
+        grid.add(new Line(9, 5, 9, 1));
+        grid.add(new Line(11, 5, 13, 13));
+        grid.add(new Line(1, 1, 6, 1));
+
+        // TODO: randomly generate lines
+        /*
         for (int i = 0; i < NUM_LINES; i++) {
             int x0 = Utils.getRand(0, grid.width);
             int y0 = Utils.getRand(0, grid.height);
             int x1 = Utils.getRand(0, grid.width);
             int y1 = Utils.getRand(0, grid.height);
-            Line l = new Line(Math.min(x0, x1), Math.min(y0, y1), Math.max(x0, x1), Math.max(y0, y1));
-            System.out.println(l);
-            grid.add(l);
+            grid.add(new Line(x0, y0, x1, y1));
         }
+         */
     }
 
     @Override
     public void render() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            Gdx.app.exit();
+        }
+
         ScreenUtils.clear(Color.BLACK);
+
         // render text
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
         spriteBatch.begin();
         grid.drawGridLabels(spriteBatch, font);
         spriteBatch.end();
+
         // render lines
         renderer.setProjectionMatrix(viewport.getCamera().combined);
         renderer.begin(ShapeRenderer.ShapeType.Line);
         grid.drawGrid(renderer);
         renderer.set(ShapeRenderer.ShapeType.Filled);
         grid.drawLinesBasic(renderer);
+        // grid.drawLinesBresenham(renderer);
+        renderer.set(ShapeRenderer.ShapeType.Line);
+        grid.drawDirectLines(renderer);
         renderer.end();
+
         viewport.apply();
     }
 
